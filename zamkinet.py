5c661ba902d31ff5630e0ad5cdd9a73d2ab9fed1
Tomasz Taraś <tomasztaras@outlook.com>
2026-02-16 13:34:24 +0100

    add scrapers for castles

---
+++ zamkinet.py
@@ -0,0 +1,216 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "beautifulsoup4>=4.14.3",
+#     "httpx>=0.28.1",
+# ]
+# [tool.uv]
+# exclude-newer = "2026-02-04T00:00:00Z"
+# ///
+
+import asyncio
+from collections.abc import Iterable
+from dataclasses import dataclass
+import json
+import re
+
+import httpx
+from bs4 import BeautifulSoup
+
+
+@dataclass(frozen=True, slots=True, kw_only=True)
+class CastleInfo:
+    name: str
+    latitude: float | None
+    longitude: float | None
+    url: str
+    state_text: str
+    state_description: str
+    entry: str
+    parking: str
+    finding_difficulty_numeric: int
+    finding_difficult_text: str
+    finding_difficult_description: str
+    last_mile_difficulty_numeric: int
+    last_mile_difficulty_text: str
+    last_mile_difficulty_description: str
+    rating_numeric: int
+    rating_text: str
+    rating_description: str
+
+
+URL_LIST = "http://zamki.net.pl/alfabetycznie.php"
+RE_LATITUDE = re.compile(r"\(([4-5]\d\.\d+)\)")
+RE_LONGITUDE = re.compile(r"\(([1-2]\d\.\d+)\)")
+
+
+def to_geojson(rows: Iterable[CastleInfo]) -> dict:
+    result = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+    for row in rows:
+        if row.longitude and row.latitude:
+            result["features"].append(dict(
+                type="Feature",
+                properties={
+                    "nazwa": row.name,
+                    "url": row.url,
+                    "stan_tekst": row.state_text,
+                    "stan_opis": row.state_description,
+                    "wstep": row.entry,
+                    "parking": row.parking,
+                    "trudnosc_odnalezienia_skala": row.finding_difficulty_numeric,
+                    "trudnosc_odnalezienia_tekst": row.finding_difficult_text,
+                    "trudnosc_odnalezienia_opis": row.finding_difficult_description,
+                    "trudnosc_dojscia_skala": row.last_mile_difficulty_numeric,
+                    "trudnosc_dojscia_tekst": row.last_mile_difficulty_text,
+                    "trudnosc_dojscia_opis": row.last_mile_difficulty_description,
+                    "ocena_skala": row.rating_numeric,
+                    "ocena_tekst": row.rating_text,
+                    "ocena_opis": row.rating_description,
+                },
+                geometry=dict(
+                    type="Point",
+                    coordinates=[row.longitude, row.latitude],
+                )
+            ))
+        else:
+            print(f"Skipping writing data for castle: {row.name} ({row.url}) due to missing coordinates.")
+    return result
+
+
+def get_list_of_castle_pages() -> list[str]:
+    response = httpx.Client().get(url=URL_LIST)
+    response.raise_for_status()
+    soup = BeautifulSoup(markup=response.text, features="html.parser")
+    div = soup.find("div", attrs={"class": "srodek-zp-srodek"})
+    assert div is not None
+    anchors = div.find_all("a", recursive=True)
+    assert anchors is not None and len(anchors) > 0
+    urls = [a["href"] for a in anchors]
+    return urls
+
+
+async def get_page_data(client: httpx.AsyncClient, url: str) -> CastleInfo:
+    # temp variables
+    name = None
+    latitude = None
+    longitude = None
+    state_text = None
+    state_description = None
+    entry = None
+    parking = None
+    finding_difficulty_numeric = None
+    finding_difficulty_text = None
+    finding_difficult_description = None
+    last_mile_difficulty_numeric = None
+    last_mile_difficulty_text = None
+    last_mile_difficulty_description = None
+    rating_numeric = None
+    rating_text = None
+    rating_description = None
+    # ---
+    response_description = await client.get(url=url, params=dict(z=1))
+    response_description.raise_for_status()
+    soup_description = BeautifulSoup(markup=response_description.text, features="html.parser")
+    title: str = soup_description.find("div", attrs={"class": "srodek-zp-gorap"}).h1.text
+    assert title is not None
+    name = title
+    div_description = soup_description.find("div", attrs={"class": "srodek-zp-srodek"})
+    assert div_description is not None
+    for row in div_description.find_all("tr"):
+        col1: str = row.find("td", attrs={"class": "opis1"}).text
+        col2 = row.find("td", attrs={"class": "opis2"}).img
+        col3: str = row.find("td", attrs={"class": "opis3"}).text
+        assert col1 is not None
+        assert col2 is not None
+        assert col3 is not None
+        match col1:
+            case "Stan zachowania:":
+                state_text = col2["alt"]
+                state_description = col3
+            case "Wstęp:":
+                entry = col3
+            case "Parking:":
+                parking = col3
+            case "Trudność odnalezienia:":
+                img_url = col2["src"]
+                img_alt = col2["alt"]
+                finding_difficulty_numeric = float(img_url[-5:-4])
+                finding_difficulty_text = img_alt
+                finding_difficult_description = col3
+            case "Trudność dojścia:":
+                img_url = col2["src"]
+                img_alt = col2["alt"]
+                last_mile_difficulty_numeric = float(img_url[-5:-4])
+                last_mile_difficulty_text = img_alt
+                last_mile_difficulty_description = col3
+            case "Subiektywna ocena:":
+                img_url = col2["src"]
+                img_alt = col2["alt"]
+                rating_numeric = float(img_url[-5:-4])
+                rating_text =img_alt
+                rating_description = col3
+    response_location = await client.get(url=url, params=dict(z=2))
+    response_location.raise_for_status()
+    soup_location = BeautifulSoup(markup=response_location.text, features="html.parser")
+    div = soup_location.find(id="licznik")
+    if div is not None:
+        coordinates = div.text
+        lat_match = RE_LATITUDE.findall(coordinates)
+        lon_match = RE_LONGITUDE.findall(coordinates)
+        if lat_match is not None and len(lat_match) == 1 and lon_match is not None and len(lon_match) == 1:
+            latitude = float(lat_match[0])
+            longitude = float(lon_match[0])
+        else:
+            print(f"Coordinates could not be parsed for castle: {name} at: {url}")
+    else:
+        print(f"Coordinates not found for castle: {name} at: {url}")
+
+    return CastleInfo(
+        name=name,
+        latitude=latitude,
+        longitude=longitude,
+        url=url,
+        state_text=state_text,
+        state_description=state_description,
+        entry=entry,
+        parking=parking,
+        finding_difficulty_numeric=finding_difficulty_numeric,
+        finding_difficult_text=finding_difficulty_text,
+        finding_difficult_description=finding_difficult_description,
+        last_mile_difficulty_numeric=last_mile_difficulty_numeric,
+        last_mile_difficulty_text=last_mile_difficulty_text,
+        last_mile_difficulty_description=last_mile_difficulty_description,
+        rating_numeric=rating_numeric,
+        rating_text=rating_text,
+        rating_description=rating_description,
+    )
+
+
+async def get_pages_data(urls: Iterable[str]) -> list[CastleInfo]:
+    limits = httpx.Limits(
+        max_connections=2,
+        max_keepalive_connections=1,
+        keepalive_expiry=5,
+    )
+    async with httpx.AsyncClient(timeout=60.0, limits=limits) as client:
+        futures = [get_page_data(client=client, url=url) for url in urls]
+        results = await asyncio.gather(*futures)
+    return results
+
+
+def main() -> None:
+    print("Hello from zamkinet.py!")
+    pages_urls = get_list_of_castle_pages()
+    print(f"Found {len(pages_urls)} pages to scrape.")
+    data = asyncio.run(get_pages_data(urls=pages_urls))
+    print("writing data to geojson file.")
+    geojson_dict = to_geojson(data)
+    with open("zamkinet.geojson", "w", encoding="utf-8") as f:
+        json.dump(geojson_dict, f, indent=2)
+
+
+if __name__ == "__main__":
+    main()
---
+++ zamkisp.py
@@ -0,0 +1,321 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "beautifulsoup4>=4.14.3",
+#     "httpx>=0.28.1",
+# ]
+# [tool.uv]
+# exclude-newer = "2026-02-04T00:00:00Z"
+# ///
+
+from collections.abc import Iterable
+from datetime import date
+import json
+import httpx
+from bs4 import BeautifulSoup
+
+from dataclasses import dataclass
+
+
+URL_LISTA_TEMPLATE = "https://zamkisp.pl/index.php?option=com_zamki&view=zamki&Itemid=63&limitstart={limitstart}"
+URL_POWIATY_TEMPLATE = "https://zamkisp.pl/index.php?option=com_powiaty&view=powiaty&Itemid=53&limitstart={limitstart}"
+URL_GMINY_TEMPLATE = "https://zamkisp.pl/index.php?option=com_gminy&view=gminy&Itemid=62&limitstart={limitstart}"
+
+DICT_WOJEWODZTWA = {
+    "B": "lubuskie",
+    "C": "łódzkie",
+    "D": "dolnośląskie",
+    "F": "pomorskie",
+    "G": "śląskie",
+    "J": "warmińsko-mazurskie",
+    "K": "podkarpackie",
+    "L": "lubelskie",
+    "M": "małopolskie",
+    "O": "podlaskie",
+    "P": "kujawsko-pomorskie",
+    "R": "mazowieckie",
+    "S": "świętokrzyskie",
+    "U": "opolskie",
+    "W": "wielkopolskie",
+    "Z": "zachodniopomorskie",
+}
+
+DICT_TYP = {
+    "M": "zniszczony",
+    "F": "pozostałości",
+    "R": "ruiny",
+    "Z": "zachowany",
+}
+
+
+@dataclass(frozen=True, slots=True, kw_only=True)
+class CastleListRow:
+    wojewodztwo: str
+    powiat: str
+    gmina: str
+    zamek_id: str
+    nazwa: str
+    typ_oryginalny: str
+    typ_interpretowany: str | None
+    szerokosc_geo: float
+    dlugosc_geo: float
+    data_wprowadzenia: date | None
+    data_aktualizacji: date | None
+    opis: str
+    url: str
+
+
+@dataclass(frozen=True, slots=True, kw_only=True)
+class CastleListRowDetails:
+    opis: str
+    szerokosc_geo: float
+    dlugosc_geo: float
+    data_wprowadzenia: date
+    data_aktualizacji: date
+    url: str
+
+
+def to_geojson(rows: Iterable[CastleListRow]) -> dict:
+    result = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+    for row in rows:
+        result["features"].append(dict(
+            type="Feature",
+            properties={
+                "wojewodztwo": row.wojewodztwo,
+                "powiat": row.powiat,
+                "gmina": row.gmina,
+                "zamek_id": row.zamek_id,
+                "nazwa": row.nazwa,
+                "typ_oryginalny": row.typ_oryginalny,
+                "typ_interpretowany": row.typ_interpretowany,
+                "data_wprowadzenia": row.data_wprowadzenia.isoformat() if row.data_wprowadzenia else None,
+                "data_aktualizacji": row.data_aktualizacji.isoformat() if row.data_aktualizacji else None,
+                "opis": row.opis,
+                "url": row.url,
+            },
+            geometry=dict(
+                type="Point",
+                coordinates=[row.dlugosc_geo, row.szerokosc_geo],
+            )
+        ))
+    return result
+
+
+def get_details(client: httpx.Client, url: str) -> CastleListRowDetails:
+    response = client.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(markup=response.text, features="html.parser")
+    main_section = soup.find(id="main_full")
+    tables = main_section.find_all("table")
+    if len(tables) < 1:
+        raise Exception("Expected at least one table in HTML.")
+    opis = ""
+    szerokosc_geo = None
+    dlugosc_geo = None
+    data_wprowadzenia = None
+    data_aktualizacji = None
+    # html is broken and missing tbody
+    for row in tables[0].find_all("tr"):
+        cells = list(row.find_all("td"))
+        if len(cells) > 2:
+            if "Opis:" in cells[1].text:
+                opis = cells[2].textarea.text.strip()
+            elif "(5x.xxxxx):" in cells[1].text:
+                szerokosc_geo = float(cells[2].text.strip())
+            elif "(1x.xxxx):" in cells[1].text:
+                dlugosc_geo = float(cells[2].text.strip())
+            elif "Data wprowadzenia:" in cells[1].text:
+                val = cells[2].text.strip()
+                if val != "0000-00-00":
+                    data_wprowadzenia = date.fromisoformat(val)
+                else:
+                    data_wprowadzenia = None
+            elif "Aktualizacja danych:" in cells[1].text:
+                val = cells[2].text.strip()
+                if val != "0000-00-00":
+                    data_aktualizacji = date.fromisoformat(val)
+                else:
+                    data_aktualizacji = None
+    return CastleListRowDetails(
+        opis=opis,
+        szerokosc_geo=szerokosc_geo,
+        dlugosc_geo=dlugosc_geo,
+        data_wprowadzenia=data_wprowadzenia,
+        data_aktualizacji=data_aktualizacji,
+        url=url,
+    )
+
+
+def get_castles(county_dict: dict[tuple[str, str], str], municipality_dict: dict[tuple[str, str, str], str]) -> list[CastleListRow]:
+    results = []
+    keep_running = True
+    offset = 0
+    step = 100
+    with httpx.Client() as client:
+        while keep_running:
+            url = URL_LISTA_TEMPLATE.format(limitstart=offset)
+            response = client.get(url=url)
+            response.raise_for_status()
+            print(f"get_castles() - offset: {offset} - Response status: {response.status_code} - url: {url}")
+            soup = BeautifulSoup(markup=response.text, features="html.parser")
+            main_section = soup.find(id="main_full")
+            tables = main_section.find_all("table")
+            if len(tables) < 2:
+                raise Exception("Expected at least two tables in HTML.")
+            # html is broken and missing tbody
+            if len(tables[1].find_all("tr", recursive=False)) == 0:
+                keep_running = False
+                break
+            for row in tables[1].find_all("tr", recursive=False):
+                kod_woj = ""
+                kod_pow = ""
+                zamek_id = ""
+                nazwa = ""
+                kod_gmi = ""
+                typ_oryginalny = ""
+                for i, val in enumerate(row.select("td")):
+                    match i:
+                        case 0:
+                            pass
+                        case 1:
+                            kod_woj = val.text.strip()
+                        case 2:
+                            kod_pow = val.text.strip()
+                        case 3:
+                            zamek_id = val.text.strip()
+                        case 4:
+                            nazwa = val.text.strip()
+                        case 5:
+                            kod_gmi = val.text.strip()
+                        case 6:
+                            pass
+                        case 7:
+                            typ_oryginalny = val.text.strip()
+                        case 8:
+                            url = "https://zamkisp.pl" + val.find("a").get("href")
+                            details = get_details(client=client, url=url)
+                            woj = DICT_WOJEWODZTWA.get(kod_woj)
+                            pow = county_dict.get((kod_woj, kod_pow))
+                            gmi = municipality_dict.get((kod_woj, kod_pow, kod_gmi))
+                            typ_interpretowany = DICT_TYP.get(typ_oryginalny)
+                            results.append(
+                                CastleListRow(
+                                    wojewodztwo=woj,
+                                    powiat=pow,
+                                    gmina=gmi,
+                                    zamek_id=zamek_id,
+                                    nazwa=nazwa,
+                                    typ_oryginalny=typ_oryginalny,
+                                    typ_interpretowany=typ_interpretowany,
+                                    szerokosc_geo=details.szerokosc_geo,
+                                    dlugosc_geo=details.dlugosc_geo,
+                                    data_wprowadzenia=details.data_wprowadzenia,
+                                    data_aktualizacji=details.data_aktualizacji,
+                                    opis=details.opis,
+                                    url=details.url,
+                                )
+                            )
+            offset += step
+    return results
+
+
+def get_counties() -> dict[tuple[str, str], str]:
+    results = {}
+    keep_running = True
+    offset = 0
+    step = 100
+    with httpx.Client() as client:
+        while keep_running:
+            url = URL_POWIATY_TEMPLATE.format(limitstart=offset)
+            response = client.get(url=url)
+            response.raise_for_status()
+            print(f"get_counties() - offset: {offset} - Response status: {response.status_code} - url: {url}")
+            soup = BeautifulSoup(markup=response.text, features="html.parser")
+            main_section = soup.find(id="main_full")
+            tables = main_section.find_all("table")
+            if len(tables) < 2:
+                raise Exception("Expected at least two tables in HTML.")
+            # html is broken and missing tbody
+            if len(tables[1].find_all("tr", recursive=False)) == 0:
+                keep_running = False
+                break
+            for row in tables[1].find_all("tr", recursive=False):
+                kod_woj = ""
+                kod_pow = ""
+                nazwa = ""
+                for i, val in enumerate(row.select("td")):
+                    match i:
+                        case 0:
+                            pass
+                        case 1:
+                            kod_woj = val.text.strip()
+                        case 2:
+                            kod_pow = val.text.strip()
+                        case 3:
+                            nazwa = val.text.strip()
+                if kod_woj and kod_pow:
+                    results[(kod_woj, kod_pow)] = nazwa
+            offset += step
+    return results
+
+
+def get_municipalities() -> dict[tuple[str, str, str], str]:
+    results = {}
+    keep_running = True
+    offset = 0
+    step = 100
+    with httpx.Client() as client:
+        while keep_running:
+            url = URL_GMINY_TEMPLATE.format(limitstart=offset)
+            response = client.get(url=url)
+            response.raise_for_status()
+            print(f"get_municipalities() - offset: {offset} - Response status: {response.status_code} - url: {url}")
+            soup = BeautifulSoup(markup=response.text, features="html.parser")
+            main_section = soup.find(id="main_full")
+            tables = main_section.find_all("table")
+            if len(tables) < 2:
+                raise Exception("Expected at least two tables in HTML.")
+            # html is broken and missing tbody
+            if len(tables[1].find_all("tr", recursive=False)) == 0:
+                keep_running = False
+                break
+            for row in tables[1].find_all("tr", recursive=False):
+                kod_woj = ""
+                kod_pow = ""
+                kod_gmi = ""
+                nazwa = ""
+                for i, val in enumerate(row.select("td")):
+                    match i:
+                        case 0:
+                            pass
+                        case 1:
+                            kod_woj = val.text.strip()
+                        case 2:
+                            kod_pow = val.text.strip()
+                        case 3:
+                            kod_gmi = val.text.strip()
+                        case 4:
+                            nazwa = val.text.strip()
+                if kod_woj and kod_pow:
+                    results[(kod_woj, kod_pow, kod_gmi)] = nazwa
+            offset += step
+    return results
+
+
+def main() -> None:
+    print("Hello from zawody.py!")
+    county_dict = get_counties()
+    print(f"Utworzono słownik powiatów ({len(county_dict)} rekordów).")
+    municipality_dict = get_municipalities()
+    print(f"Utworzono słownik gmin ({len(municipality_dict)} rekordów).")
+    data = get_castles(county_dict=county_dict, municipality_dict=municipality_dict)
+    geojson_dict = to_geojson(data)
+    with open("zamkisp.geojson", "w", encoding="utf-8") as f:
+        json.dump(geojson_dict, f, indent=2)
+
+
+if __name__ == "__main__":
+    main()
